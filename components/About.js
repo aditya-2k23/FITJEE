47af9e045e90f13da7317bde8d05c4d791389025
aditya-2k23 <holaaditya123@gmail.com>
2024-11-08 00:28:01 +0530

    Add About component for program description

---
+++ components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container mb-40">
+      <h1 className="text-5xl font-bold">About the Program</h1>
+    </div>
+  );
+};
+
+export default About;
