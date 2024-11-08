c48056159eaa036a6c9ed8aed5385cd6106806b6
aditya-2k23 <holaaditya123@gmail.com>
2024-11-09 00:30:57 +0530

    Add Main component to Home page layout and adjust padding

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import About from "@/components/About";
 import BreadCrumbs from "@/components/BreadCrumbs";
 import Hero from "@/components/Hero";
+import Main from "@/components/Main";
 
 export default function Home() {
   return (
-    <div className="container h-screen px-32 py-16 bg-[rgb(239,242,245)]">
+    <div className="container h-screen px-36 py-16 bg-[rgb(239,242,245)]">
       <BreadCrumbs />
       <Hero />
       <About />
+      <Main />
     </div>
   );
 }
