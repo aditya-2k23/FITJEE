44e6de2452a7c09bfb47432d617d075429028362
aditya-2k23 <holaaditya123@gmail.com>
2024-11-09 00:30:48 +0530

    Add custom gray colors to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
     extend: {
       colors: {
         primary: "rgb(196, 23, 27)",
+        gray: {
+          900: "#141414",
+          400: "rgb(190, 185, 174)",
+        },
       },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
