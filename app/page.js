e731a6507e2c99b7e625d619131b53dd01c23463
aditya-2k23 <holaaditya123@gmail.com>
2024-11-20 13:50:38 +0530

    Update padding and overflow properties in Home component for improved layout and responsiveness

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Main from "@/components/Main";
 
 export default function Home() {
   return (
-    <div className="container h-screen px-36 py-16 bg-[rgb(239,242,245)]">
+    <div className="container h-screen overflow-x-hidden pl-10 pr-8 md:px-36 md:py-16 bg-[rgb(239,242,245)]">
       <BreadCrumbs />
       <Hero />
       <About />
